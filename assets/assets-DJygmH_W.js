c7ca965782b809d110b4d45589f75b974e3e8fb7
CGB-ENERGIA <CGB-ENERGIA@users.noreply.github.com>
2026-08-01 15:03:12 +0000

    deploy: 040378b0e461f96690f9358c30e56efa060d4a67

---
+++ assets/assets-DJygmH_W.js
@@ -0,0 +1,1 @@
+function e(e){return`/consumidores-web/${e.replace(/^\//,``)}`}export{e as t};
--- assets/pinia-Cbw1tadE.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index-nw3st4Bq.js";var t=e(({app:e,store:t})=>{e.use(t)});export{t as default};
